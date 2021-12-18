dc885ccbfd5737382e96194e988b2c2f162a8db9
WePlayFleX <alex.kamai@ya.ru>
2021-12-18 23:51:35 +0300

    Добавьте файлы проекта.

---
+++ Home Work 26.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char strMass[80];
+    int word = 0, count = 0;
+  
+    cout << "Enter a line of text: " << endl;
+    gets_s(strMass, 80);
+
+    int i = 0;
+    while (strMass[i] == ' ' && strMass[i] != '\0')
+    {
+        i++;
+    }
+    
+    while (strMass[i] != '\0') 
+    {
+        if (strMass[i] != ' ' && word == 0)
+        {
+            word = 1;
+            count++;
+        }
+        else if (strMass[i] == ' ')
+        {
+            word = 0;
+        }
+        i++;
+    }
+    cout << "Number of words per line -  " << count << endl;
+    system("pause");
+    return 0;
+}
